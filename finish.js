433be950fff14f0554719d3034c82693e8ed3af4
Cyrille <cyrillechateau@icloud.com>
2023-03-12 23:38:15 +0100

    my code

---
+++ finish.js
@@ -0,0 +1,7 @@
+const args = process.argv.slice(2);
+
+function win(a){
+  console.log("J'ai finalement terminé les épreuves de la terre, c'était parfois compliqué et même si j'ai déjà des connaissances en code, ça m'a fait du bien de (re)voir ma logique.")
+}
+
+win(args[0]);
